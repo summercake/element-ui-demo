cdd1bc4089ef83a0b492a9b25eccb5e5f55c0d81
summercake@qq.com <summercake@qq.com>
2018-08-24 00:21:23 -0500

    element-ui

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <leftNav/>
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+  import leftNav from '@/components/common/leftNav'
+
+  export default {
+    name: 'App',
+    components: {
+      leftNav
+    }
+  }
+</script>
+<style>
+  #app{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2C3E50;
+  }
+  .main{
+    float: left;
+    width: 95%;
+    background-color: #EFF2F7;
+    overflow: hidden;
+  }
+</style>
---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="left-nav">
+    <ul>
+      <li>
+        <i class="icon iconfont icon-goumai"></i>
+        <div>收银</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-dianpu"></i>
+        <div>店铺</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-hanbao"></i>
+        <div>商铺</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-huiyuanqia"></i>
+        <div>会员</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-zuanshi"></i>
+        <div>统计</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-gongnengjianyi"></i>
+        <div>设置</div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'leftNav'
+  }
+</script>
+<style>
+  .left-nav{
+    float: left;
+    color: #FFFFFF;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1D8CE0;
+    width: 5%;
+  }
+  .iconfont{
+    font-size: 24px;
+  }
+  .left-nav ul{
+    padding: 0;
+    margin: 0;
+  }
+  .left-nav li{
+    list-style:none;
+    text-align: center;
+    border-bottom: 1px solid #20a0ff;
+    padding: 10px;
+  }
+</style>
